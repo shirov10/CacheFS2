d8dc51bb93c84b50e778743ec823341bdca0da27
omri <omri.bloch@gmail.com>
2017-06-10 16:24:04 +0300

    FBR_works

--- CacheFS.cpp
+++ CacheFS.cpp
@@ -41,7 +41,7 @@ int CacheFS_init(int blocks_num, cache_algo_t cache_algo, double f_old , double 
         _cache=new Cache_FBR(blocks_num,f_old,f_new);
     }
 
-    _cache->simpletest1(20);
+//    _cache->simpletest1(20);
 
     return 0;
 
